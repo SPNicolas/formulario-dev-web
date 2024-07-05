47b6306ad3d6e70bf11fa846c689abb6779289a6
SPNicolas <nicolassteffenspereira@gmail.com>
2024-07-05 13:18:34 -0300

    formulário iniciado

---
+++ src/formulario.vue
@@ -0,0 +1,35 @@
+<script setup>
+  import { ref } from 'vue';
+  const senha = ref('');
+  const nome = ref('');
+  const idade = ref('');
+  const aceitaTermos = ref(false);
+</script>
+
+<template>
+  <div class="formulario">
+    <div class="pessoal">
+      <input type="text" v-model="nome" placeholder="Digite seu nome" />
+      <label for=""></label>
+    </div>
+
+
+  </div>
+  
+</template>
+
+
+<style scoped>
+    .formulario {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border: 1px solid white;
+    }
+    .formulario .pessoal {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+</style>
